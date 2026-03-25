a97cd2bd77c35dc435c03332976f2dd07536e49b
shashank-kumar-uv <shashank.inbox2020@gmail.com>
2026-03-25 14:08:46 +0530

    useMemo,useCallBack

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,23 +1,45 @@
 import { useUsers } from "@/context/UserContext";
+import { User } from "@/types/user";
 import { useRouter } from "expo-router";
+import React, { useCallback } from "react";
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 
+interface UserCardProps{
+    user:User;
+    onPress:()=>void;
+}
+const UserCard=React.memo(({user, onPress}:UserCardProps)=>{
+    return(
+        <TouchableOpacity style={styles.userCard} onPress={onPress}>
+
+                        <Text style={styles.name}>{user.name}</Text>
+                        <Text style={styles.email}>{user.email}</Text>
+                    </TouchableOpacity>
+    )
+})
+
 export default function Home(){
-    // const {data:users,loading,error}=useFetch<User[]>("https://jsonplaceholder.typicode.com/users")
     const { users, loading, error } = useUsers();
     const router=useRouter();
-
+    const renderItem = useCallback(({ item }: { item: User }) => (
+        <UserCard
+            user={item}
+            onPress={() => router.push(`/profile?name=${item.name}&role=${item.company.name}`)}
+        />
+    ), [router]);
     return(
         loading?<View style={styles.container}><Text>Loading...</Text></View>:
         error?<View style={styles.container}><Text>Error Occured</Text></View>:
                 <View style={styles.container}>
-                    <FlatList data={users} keyExtractor={(item) => `${item.id}`} renderItem={({ item }) => (
-                        <TouchableOpacity style={styles.userCard} onPress={() =>router.push(`/profile?name=${item.name}&role=${item.company.name}`)}>
-
-                        <Text style={styles.name}>{item.name}</Text>
-                        <Text style={styles.email}>{item.email}</Text>
-                    </TouchableOpacity>)} />
+                    <FlatList
+                        initialNumToRender={10}
+                        maxToRenderPerBatch={10}
+                        windowSize={5}
+                        removeClippedSubviews={true}
+                        data={users}
+                        keyExtractor={(item) => `${item.id}`}
+                        renderItem={renderItem} />
         </View>
     )
 }
