06eb12cf4977d940d4340c2dce20f9e9922e32f4
shashank-kumar-uv <shashank.inbox2020@gmail.com>
2026-03-25 13:03:28 +0530

    settings file added

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,22 +1,60 @@
-import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function SettingsScreen() {
-    return(
+    const [name, setName] = React.useState<string>("");
+    const [savedName, setSavedName] = React.useState<string>("");
+    useEffect(() => {
+        const fetchUserName = async () => {
+            const storedName: string | null = await AsyncStorage.getItem("userName");
+            if (storedName) {
+                setSavedName(storedName);
+            }
+        }
+        fetchUserName();
+    }, [])
+    return (
         <View style={styles.container}>
             <Text style={styles.name}>Settings Screen</Text>
+            <TextInput placeholder="Type something..." style={styles.btn1}
+                onChangeText={(text: string) => setName(text)}
+            />
+            <TouchableOpacity onPress={async () => {
+                await AsyncStorage.setItem("userName", name);
+                setSavedName(name);
+            }} style={styles.btn}>
+                <Text style={styles.btnText}>Submit</Text>
+            </TouchableOpacity>
+            {savedName && <Text>Welcome, {savedName}!</Text>}
         </View>
     )
 }
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
     },
-    name:{
-        fontSize:20,
-        fontWeight:"bold",
-        marginBottom:10,
+    name: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    btn: {
+        marginTop: 20,
+        backgroundColor: "#007AFF",
+        padding: 10,
+        borderRadius: 5
+    },
+    btn1: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        padding: 10,
+        width: "80%"
+    },
+    btnText:{
+        color: "#fff",
     }
 })
